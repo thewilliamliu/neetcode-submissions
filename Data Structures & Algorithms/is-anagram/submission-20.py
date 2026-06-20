98d68027ecad66962b83b45f2034a15da499f790
William Liu <realwilliamliu@gmail.com>
2026-06-20 16:03:50 -0400

    Add: is-anagram - submission-20

---
+++ Data Structures & Algorithms/is-anagram/submission-20.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS = Counter(s)
+        countT = Counter(t)
+
+        if countS == countT:
+            return True
+
+        return False
+
+        
