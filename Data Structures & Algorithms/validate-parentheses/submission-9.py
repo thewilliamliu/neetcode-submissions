56555ee44a299f19b2a416c793ba4da8c1d03f50
William Liu <realwilliamliu@gmail.com>
2026-08-14 03:18:37 -0400

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        ## You can declare a hash map like this. Need commas.
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        for i, x in enumerate(s):
+            if x == '(' or x == '{' or x == '[':
+                stack.append(x)
+
+            if (x == ')' or x == '}' or x == ']') and not stack:
+                return False
+
+            if x in pairs and pairs[x] != stack[-1]:
+                return False
+            elif x in pairs:
+                stack.pop()
+            
+        return not stack
