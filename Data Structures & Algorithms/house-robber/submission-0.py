fa4bba9ea39b293b5f8e46fd3933193190a3b7df
William Liu <realwilliamliu@gmail.com>
2026-08-17 17:03:48 -0700

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        profit = [0] * len(nums)
+    
+        for i, cash in enumerate(nums):
+            if i == 0:
+                profit[i] = nums[0]
+            elif i == 1:
+                profit[i] = max(nums[0], nums[1])
+            else:
+                profit[i] = max(profit[i-1], profit[i-2] + nums[i])
+        
+        return profit[len(nums) - 1]
